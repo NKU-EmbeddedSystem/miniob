70b0d62c129197517517aa0e62f898baa5c92c72
jz <jiangzhangsg@qq.com>
2021-11-08 20:55:28 +0800

    add some tests to nullable.sql, insert and selects done, leave compare to do

--- test/nullable.sql
+++ test/nullable.sql
@@ -1,4 +1,20 @@
 drop table t1;
-create table t1 (id int not null, age int not null, address nullable); 
-create table t1 (id int, age int, address char nullable); 
-insert into t1 values(1,1, null);
+create table t(id int, num int nullable, birthday date nullable);
+create index i_num on t(num);
+insert into t values(1, 2, '2020-01-01');
+insert into t values(1, null, null);
+insert into t values(1, null, '2020-02-02');
+insert into t values(null, 1, '2020-01-02');
+select * from t;
+
+select * from t where id is null;
+select * from t where id is not null;
+select * from t where num is null; 
+select * from t where num > null;
+select * from t where num <> null;
+select * from t where 1=null;
+select * from t where 'a'=null;
+select * from t where null = null;
+select * from t where null is null;
+select * from t where '2020-01-31' is null;
+
