35c6bd106e7088dcf1696184f490edfb2f6ebc54
JerryMouseZ <1711333@mail.nankai.edu.cn>
2021-11-15 08:55:29 +0800

    implement hash join for equal condition

---
+++ test/join.sql
@@ -0,0 +1,2 @@
+# select t1.age, t2.name, t1.id from t1 inner join t2 on t1.id = t2.id;
+select * from t1, t2;
---
+++ test/join_install.sql
@@ -0,0 +1,6 @@
+drop table t1;
+drop table t2;
+create table t1(id int, age int);
+create table t2(id int, name CHAR);
+insert into t1 values(1, 2), (3, 4), (1, 3);
+insert into t2 values(1, "A"), (3, "B");
