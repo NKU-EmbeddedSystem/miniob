a27f045399b2fefa29791b425cbb27506410da98
JerryMouseZ <1711333@mail.nankai.edu.cn>
2021-11-21 11:46:00 +0800

    not trx

--- src/observer/session/session.cpp
+++ src/observer/session/session.cpp
@@ -45,8 +45,8 @@ bool Session::is_trx_multi_operation_mode() const {
 
 Trx *Session::current_trx() {
   return nullptr;
-//  if (trx_ == nullptr) {
-//    trx_ = new Trx;
-//  }
-//  return trx_;
+  if (trx_ == nullptr) {
+    trx_ = new Trx;
+  }
+  return trx_;
 }
